cb18f256506397d981eb8604bb5f23553069412d
donggggg <dongggyu.kim@webtoonscorp.com>
2023-03-02 10:29:26 +0900

    ProgressBar 컴포넌트 개발

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,10 +3,46 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper size={size} aria-valuenow={value}>
+      <Container>
+        <Progress size={size} value={value} />
+      </Container>
+    </Wrapper>
+  );
 };
+
+const styleLookup = {
+  small: {
+    height: '8px'
+  },
+  medium: {
+    height: '12px'
+  },
+  large: {
+    height: '16px'
+  }
+};
+
+const Wrapper = styled.div`
+  width: 370px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+  padding: ${p => p.size === 'large' && '4px'};
+  border-radius: 8px;
+`;
+
+const Container = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Progress = styled.div`
+  width: ${p => p.value + '%'};
+  height: ${p => styleLookup[p.size].height};
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
